e953cc5c3458ab6412ec3def761c17252f03f9d4
Jerenemy <jzay@wesleyan.edu>
2026-02-11 02:24:41 -0500

    removed popup at beginning of launching app

--- Chess/src/com/jeremyzay/zaychess/App.java
+++ Chess/src/com/jeremyzay/zaychess/App.java
@@ -1,8 +1,5 @@
 package com.jeremyzay.zaychess;
 
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
 import com.jeremyzay.zaychess.view.gui.MainFrame;
@@ -23,26 +20,8 @@ public class App {
 
         try {
             SwingUtilities.invokeLater(() -> {
-                // Initialize MainFrame immediately to capture instance?
-                // The loading dialog logic is fine.
-
-                JDialog loadingDialog = new JDialog((JFrame) null, "Lade Schach...", true);
-                JProgressBar progressBar = new JProgressBar();
-                progressBar.setIndeterminate(true);
-                loadingDialog.add(progressBar);
-                loadingDialog.setSize(300, 75);
-                loadingDialog.setLocationRelativeTo(null);
-                loadingDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-
-                new Thread(() -> {
-                    ResourceLoader.preload();
-                    SwingUtilities.invokeLater(() -> {
-                        loadingDialog.dispose();
-                        new MainFrame().setVisible(true);
-                    });
-                }).start();
-
-                loadingDialog.setVisible(true);
+                ResourceLoader.preload();
+                new MainFrame().setVisible(true);
             });
         } catch (Exception e) {
             javax.swing.JOptionPane.showMessageDialog(null, e.toString(), "Startup error",
